eb6bdd8880104c6adfeaae0663081115fc0f28da
czq01 <czq01@github.com>
2021-05-17 17:21:53 +0800

    --

--- bak.cpp
+++ bak.cpp
@@ -7,6 +7,7 @@
 
 const int backup_interval[2]{30,180};
 const char* backup_column[2]{"datet","hdatet"};
+const char * folder[2]{"root","home"};
 
 bool mysql_initialize(MYSQL ** connect) {
     *connect = mysql_init (NULL);
@@ -24,19 +25,24 @@ bool mysql_initialize(MYSQL ** connect) {
 
 
 int backup(MYSQL ** mysql, int choice) {
-    char *backup_command, *sql_command = NULL; 
-    char * inqury_command = (char *)malloc(50*sizeof(inqury_command));
-    sprintf(inqury_command,"SELECT MAX(%s) from backup_time",backup_column[choice]);
     own::Date a; a.set_today();
     int datet = (a.year-2000)*365 +a.month*30+a.day; 
-    mysql_query(*mysql,inqury_command);
-    MYSQL_RES * result = mysql_use_result(*mysql);
-    MYSQL_ROW row = mysql_fetch_row(result);
-    char * last_t = row[0];
-    int last_time = atoi(last_t);
-    mysql_free_result(result);
+    int last_time;
+
+    {
+        // inquiry_function
+        char * inqury_command = (char *)malloc(50*sizeof(inqury_command));
+        sprintf(inqury_command,"SELECT MAX(%s) from backup_time",backup_column[choice]);
+        mysql_query(*mysql,inqury_command);
+        MYSQL_RES * result = mysql_use_result(*mysql);
+        MYSQL_ROW row = mysql_fetch_row(result);
+        char * last_t = row[0];
+        int last_time = atoi(last_t);
+        mysql_free_result(result);
+    }
 
     if (datet - last_time >= backup_interval[choice]) {
+        char *backup_command, *sql_command = NULL; 
         backup_command = (char *)malloc(200*sizeof(backup_command));
         sql_command = (char *)malloc(100*sizeof(sql_command));
         if (choice) sprintf(backup_command, "tar cpzf /mnt/h/onedrive/backup/root/backup_%s.tgz --exclude=/proc --exclude=/lost+found --exclude=/mnt --exclude=/home --exclude=/lost+found --exclude=/sys --exclude=/media --exclude=/run /",a.date);
@@ -68,14 +74,15 @@ int if_task_exists() {
 }
 
 
-int del_backup() {
-    char num;
-    char *date, *command= NULL;
+int del_backup(int choice) {
+    char num; char *date, *command= NULL;
     int num_1;
     FILE *f;
     date = (char *)malloc(15*sizeof(char*));
     command = (char *)malloc(50*sizeof(char*));
-    f= popen("stat /mnt/h/backup/backup_* | awk '/^Modify/{print $2}' | awk -F- '{if(NR==1)print $1$2$3} END{print NR}'","r");
+    char * inquiry = (char *)malloc(150*sizeof(char*));
+    sprintf(inquiry,"stat /mnt/h/Onedrive/backup/%s/backup_* | awk '/^Modify/{print $2}' | awk -F- '{if(NR==1)print $1$2$3} END{print NR}'",folder[choice]);
+    f= popen(inquiry,"r");
     fgets(date,9,f);
     fgets(&num,2,f);
     fgets(&num,2,f);
@@ -83,16 +90,17 @@ int del_backup() {
     f = NULL;
     num_1 = num -'0';
     if (num_1 > 2) {
-        sprintf(command,"rm -rf /mnt/h/backup/backup_%s.tgz",date);
+        sprintf(command,"rm -rf /mnt/h/Onedrive/backup/%s/backup_%s.tgz",folder[choice],date);
         system(command);
     }
     free(command); command=NULL;
     free(date);date = NULL;
+    free(inquiry);inquiry=NULL;
     return 0;
 }
 
 
-int main(int args, char argv[]) {
+int main(int args, char * argv[]) {
     setuid(0);
     /*
     if (if_task_exists()) {
@@ -100,7 +108,11 @@ int main(int args, char argv[]) {
         // exit(-1);
     }*/
     int choice = 0;
-    if (args) {choice = argv[0];}
+    if (args) {
+        char * a = argv[0];
+        if (a[0]=='-' && a[1]=='h') {choice=1;}
+        else if (a[0]=='-' && a[1]=='r') {choice=0;}
+    }
     const unsigned int sleep_time = 60*60*24*3;
     MYSQL *mysql = NULL;
     bool value;
@@ -112,7 +124,7 @@ int main(int args, char argv[]) {
     backup(&mysql, choice);
     mysql_close(mysql);
     mysql= NULL;
-    del_backup();
+    del_backup(choice);
     //sleep(sleep_time);
     return 0;
 }
